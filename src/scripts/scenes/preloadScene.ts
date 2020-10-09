d158af2b7c4cb18947136a51e6075deaa156c1ef
RNy <rny@ims.ch>
2020-10-09 20:35:53 +0200

    TeamNames are received from Backend

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -1,4 +1,6 @@
 import {config, gameState} from '../game';
+import GameInfoService from '../services/gameInfoService';
+import {Team} from '../services/teams.model';
 
 export default class PreloadScene extends Phaser.Scene {
   constructor() {
@@ -8,6 +10,13 @@ export default class PreloadScene extends Phaser.Scene {
   preload() {}
 
   create() {
+    //GameInfoService --> Get Teams
+    new GameInfoService().getGames().then(games => {
+      this.buildTeamSide(games[0].homeTeam, games[0].awayTeam);
+    });
+  }
+
+  private buildTeamSide(homeTeam: Team, awayTeam: Team) {
     const width = window.innerWidth * window.devicePixelRatio as number;
     const height = window.innerHeight * window.devicePixelRatio as number;
 
@@ -15,7 +24,8 @@ export default class PreloadScene extends Phaser.Scene {
     button.setInteractive();
     button.on('pointerdown', ev =>
         {
-          gameState.selectedTeam = 'YB';
+          gameState.selectedTeam = homeTeam.name;
+          //GameStartService -> Start Game with Team Name
           this.scene.start('MainScene');
         }
     );
@@ -24,13 +34,13 @@ export default class PreloadScene extends Phaser.Scene {
     button2.setInteractive();
     button2.on('pointerdown', ev =>
         {
-          gameState.selectedTeam = 'FCB';
+          gameState.selectedTeam = awayTeam.name;
+          //GameStartService -> Start Game with Team Name
           this.scene.start('MainScene');
         }
     );
 
-    //this.add.text((width / 4) - 20, (height / 2) - 20, 'YB', { fontSize: '10em', color: '#000', fontFamily: '"ComicSans MS"' });
-    this.add.text((config.scale?.width as number / 4) - 100, (config.scale?.height as number / 2) - 100, 'YB', { fontSize: '10em', color: '#000', fontFamily: '"ComicSans MS"' });
-    this.add.text(((config.scale?.width as number / 4) * 3) - 100, (config.scale?.height as number / 2) - 100, 'FCB', { fontSize: '10em', color: '#000', fontFamily: '"ComicSans MS"' });
+    this.add.text((config.scale?.width as number / 4) - 100, (config.scale?.height as number / 2) - 50, homeTeam.name, { fontSize: '6em', color: '#000', fontFamily: '"ComicSans MS"' });
+    this.add.text(((config.scale?.width as number / 4) * 3) - 100, (config.scale?.height as number / 2) - 50, awayTeam.name, { fontSize: '6em', color: '#000', fontFamily: '"ComicSans MS"' });
   }
 }
